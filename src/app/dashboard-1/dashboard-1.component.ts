2c2b5543351ca9dc9d28a138125a795b56bb6ea5
Warat Narattharaksa <warat@192.168.1.80>
2024-07-16 09:35:52 +0700

    initial

---
+++ src/app/dashboard-1/dashboard-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-1.component.html',
+  styleUrl: './dashboard-1.component.css'
+})
+export class Dashboard1Component {
+
+}
