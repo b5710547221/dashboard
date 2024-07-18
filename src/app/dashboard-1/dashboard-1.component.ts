3e535818d4a0ed5247beddf70cf689757d18e084
Warat Narattharaksa <warat@Warats-MacBook-Pro.local>
2024-07-19 03:24:00 +0700

    update

--- src/app/dashboard-1/dashboard-1.component.ts
+++ src/app/dashboard-1/dashboard-1.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { DatePipe,CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard-1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './dashboard-1.component.html',
-  styleUrl: './dashboard-1.component.css'
+  styleUrl: './dashboard-1.component.css',
+  providers: [DatePipe]
 })
 export class Dashboard1Component {
-
+  currentDate = new Date();
+ 
 }
--- src/app/dashboard-2/dashboard-2.component.ts
+++ src/app/dashboard-2/dashboard-2.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { DatePipe,CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './dashboard-2.component.html',
-  styleUrl: './dashboard-2.component.css'
+  styleUrl: './dashboard-2.component.css',
+  providers: [DatePipe]
 })
 export class Dashboard2Component {
-
+  currentDate = new Date();
 }
