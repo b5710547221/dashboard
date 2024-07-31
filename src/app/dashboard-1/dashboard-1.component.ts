2198e1439d2ab2084e7b7e4d390a48cd4a05f432
Warat Narattharaksa <warat@192.168.1.64>
2024-07-31 13:08:30 +0700

    test

--- src/app/dashboard-1/dashboard-1.component.ts
+++ src/app/dashboard-1/dashboard-1.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
 import { DatePipe,CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription, timer } from "rxjs";
+import { map, share } from "rxjs/operators";
 
 @Component({
   selector: 'app-dashboard-1',
@@ -13,5 +14,50 @@ import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angul
 })
 export class Dashboard1Component {
   currentDate = new Date();
- 
+  
+/*    time :any = new Date()
+    
+  getCurrentDate() {
+  setInterval(() => {
+  this.time = new Date().toLocaleString; 
+  }, 1000);
+  } */
+  time = new Date();
+    rxTime = new Date();
+    intervalId :any;
+    subscription: Subscription = new Subscription;
+  
+    ngOnInit() {
+      // Using Basic Interval
+      this.intervalId = setInterval(() => {
+        this.time = new Date();
+      }, 1000);
+  
+      // Using RxJS Timer
+      this.subscription = timer(0, 1000)
+        .pipe(
+          map(() => new Date()),
+          share()
+        )
+        .subscribe(time => {
+          let hour = this.rxTime.getHours();
+          let minuts = this.rxTime.getMinutes();
+          let seconds = this.rxTime.getSeconds();
+          //let a = time.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+          let NewTime = hour + ":" + minuts + ":" + seconds
+          console.log(NewTime);
+          this.rxTime = time;
+        });
+    }
+  
+    ngOnDestroy() {
+      clearInterval(this.intervalId);
+      if (this.subscription) {
+        this.subscription.unsubscribe();
+      }
+    }
 }
+
+ 
+    
+
--- src/app/dashboard-2/dashboard-2.component.ts
+++ src/app/dashboard-2/dashboard-2.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
 import { DatePipe,CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription, timer } from "rxjs";
+import { map, share } from "rxjs/operators";
 @Component({
   selector: 'app-dashboard-2',
   standalone: true,
@@ -10,6 +12,47 @@ import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angul
   styleUrl: './dashboard-2.component.css',
   providers: [DatePipe]
 })
-export class Dashboard2Component {
-  currentDate = new Date();
+export class Dashboard2Component implements OnInit, OnDestroy  {
+   currentDate = new Date();
+   /*  time :any = new Date()
+    
+    getCurrentDate() {
+    setInterval(() => {
+    this.time = new Date().toLocaleString; 
+    }, 1000);
+    } */
+    time = new Date();
+    rxTime = new Date();
+    intervalId :any;
+    subscription: Subscription = new Subscription;
+  
+    ngOnInit() {
+      // Using Basic Interval
+      this.intervalId = setInterval(() => {
+        this.time = new Date();
+      }, 1000);
+  
+      // Using RxJS Timer
+      this.subscription = timer(0, 1000)
+        .pipe(
+          map(() => new Date()),
+          share()
+        )
+        .subscribe(time => {
+          let hour = this.rxTime.getHours();
+          let minuts = this.rxTime.getMinutes();
+          let seconds = this.rxTime.getSeconds();
+          //let a = time.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+          let NewTime = hour + ":" + minuts + ":" + seconds
+          console.log(NewTime);
+          this.rxTime = time;
+        });
+    }
+  
+    ngOnDestroy() {
+      clearInterval(this.intervalId);
+      if (this.subscription) {
+        this.subscription.unsubscribe();
+      }
+    }
 }
