2c8f5f499418c310025eea9544c9cf22f2244fd6
Kunagorn Sirikupt <kunagorn.sirikupt@gmail.com>
2024-08-19 12:36:32 +0700

    Remove test component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { Dashboard2Component } from './dashboard-2/dashboard-2.component';
 import { AppComponent } from './app.component';
 import { Dashboard1Component } from './dashboard-1/dashboard-1.component';
-import { JoyceComponent } from './joyce/joyce.component';
 
 //export const routes: Routes = [];
 export const routes: Routes = [
-    { path: 'dash1', component: Dashboard1Component },
-    { path: 'dash2', component: Dashboard2Component },
-    { path: 'joyce', component: JoyceComponent }
-  ];
+  { path: 'dash1', component: Dashboard1Component },
+  { path: 'dash2', component: Dashboard2Component },
+];
